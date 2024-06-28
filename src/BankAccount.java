259a2365c32cd49973c8c46907c71c405ed86251
jacowci61 <jacowci61@gmail.com>
2024-06-28 17:12:35 +0300

    Initial commit

---
+++ src/BankAccount.java
@@ -0,0 +1,4 @@
+public class BankAccount {
+    private double AmountOfMoney;
+    // basically make data private (unaccessible) and make getters/setters to access it
+}
---
+++ src/CreditCard.java
@@ -0,0 +1,5 @@
+public class CreditCard extends BankAccount {
+    private int CreditCardNumber;
+    private int PIN;
+    private int AmountOfIncorrectPINEntered;
+}
