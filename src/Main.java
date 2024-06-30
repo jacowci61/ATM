c02ef5fc6f54ad8ad75a4b1640d3957538485a24
jacowci61 <jacowci61@gmail.com>
2024-06-30 20:34:50 +0300

    Direct changes of data in lines[], without creating and editing class objects

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 
 public class Main {
@@ -11,25 +9,25 @@ public class Main {
     public static void main(String[] args) {
 
         File directory = new File("./");
+        System.out.println("\n");
         System.out.println("path is: " + directory.getAbsolutePath());
 
         String filePath = "src/ATMData.txt";
         String filePathTEST = "src/ATMDataTEST.txt";
 
-        // Using BufferedReader to read the file
         try (BufferedReader br = new BufferedReader(new FileReader(filePathTEST))) {
             String line;
+            System.out.println("\n");
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        BankAccount bankAccount1 = new BankAccount("u123ADDED",2550.0);
-        CreditCard creditCard1 = new CreditCard(bankAccount1,4111522263337444L, 1024, false);
+        BankAccount bankAccount1 = new BankAccount("u123ADDED", 2550.0);
+        CreditCard creditCard1 = new CreditCard(bankAccount1, 4111522263337444L, 1024, false);
 
         String str1 = creditCard1.getBankAccountID() + " " + creditCard1.getAmountOfMoney() + " " + creditCard1.getCreditCardNumber() + " " + creditCard1.getPIN() + " " + creditCard1.getIsCreditCardBlocked();
-
 
         /* appending content to .txt
 
@@ -40,32 +38,75 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-         */
-
+        */
 
         try {
             String[] lines = readFileIntoArray(filePathTEST);
+
+            System.out.println("\n");
             for (String line : lines) {
                 System.out.println("Lines in txt file are: " + line);
             }
+
             String strTEST = lines[0];
+            System.out.println("\n");
             System.out.println(strTEST);
             String[] TempObject = strTEST.split("\\s+");
-            for (String object : TempObject){
+
+            for (String object : TempObject) {
                 System.out.println("Properties: " + object);
             }
 
-            String query = "4789566628948954";
-            String result = findElementContainingSequence(lines, query);
+            //add reading user input here. Credit card number can be entered with spaces or not, doesnt matter.
+            //use handling input from b64 converter/somewhere in other project on github
 
+
+            String query = "4111522263337445";
+            String result = findElementContainingSequence(lines, query);
+            int IndexOfResult = Arrays.asList(lines).indexOf(result);
+            System.out.println("Result is:" + result + "; and extraction is " + lines[IndexOfResult]);
+            // unclear whether to directly modify object, skipping finding precise string from lines
+            // and just keeping the index of found string
+            // see [[note# already read objects handling]]
+            String[] TempObject2 = result.split("\\s+");
+            // if:
+            // either card balance changed
+            // or card becomes blocked
+            // store these two methods in their respective classes+setters?
+
+            // TempObject2[1] = where i face problems. Maybe each string in lines[] should be
+            // constructed to an object so i can use setters. But that's unneeded dealing
+            // with IO-operations. Can make two variants, for now it'll be a direct rewrite w/o setters
+            // On a second thought - why use lines[]? Because its easiest way to handle searching for element
+
+            for (String object : TempObject2) {
+                System.out.println("Before: " + object);
+            }
+            TempObject2[1] = String.valueOf(4900.0);
+            TempObject2[4] = String.valueOf(true); // setting that toggles ifcardisblocked
+            for (String object : TempObject2) {
+                System.out.println("After: " + object);
+            }
+            System.out.println("\n");
             if (result != null) {
                 System.out.println("Matching element: " + result);
             } else {
                 System.out.println("No matching element found.");
             }
-        }
-        catch (IOException e) {
+
+
+            // problem here is that i dont replace data in lists
+            // so, before calling a setter, find a string with creditcard number to be replaced and store rplcmnt = index of this string
+            // .split("\\s+"), replace needed parameters of object, concat and replace lines[rplcmt] with newly concated string
+            creditCard1.setPIN(9876);
+            creditCard1.setCreditCardNumber(1111222233334444L);
+            //String str2 = creditCard1.getBankAccountID() + " " + creditCard1.getAmountOfMoney() + " " + creditCard1.getCreditCardNumber() + " " + creditCard1.getPIN() + " " + creditCard1.getIsCreditCardBlocked();
+            //System.out.println("\n" + str2);
+
+            overwriteFile(filePathTEST, lines);
+            System.out.println("\n");
+            System.out.println("File overwritten successfully.");
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -78,17 +119,16 @@ public class Main {
          */
     }
 
-
-        public static String[] readFileIntoArray(String filePath) throws IOException {
-            List<String> linesList = new ArrayList<>();
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    linesList.add(line);
-                }
+    public static String[] readFileIntoArray(String filePath) throws IOException {
+        List<String> linesList = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                linesList.add(line);
             }
-            return linesList.toArray(new String[0]);
         }
+        return linesList.toArray(new String[0]);
+    }
 
 
     public static String findElementContainingSequence(String[] data, String query) {
@@ -99,33 +139,13 @@ public class Main {
         }
         return null;
     }
-/*  overwriting file
 
-    public class OverwriteFileWithArray {
-        public static void main(String[] args) {
-            String filePath = "example.txt";
-            String[] linesToWrite = {
-                    "This is the first line.",
-                    "This is the second line.",
-                    "This is the third line."
-            };
-
-            try {
-                overwriteFileWithArray(filePath, linesToWrite);
-                System.out.println("File has been overwritten successfully.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public static void overwriteFileWithArray(String filePath, String[] lines) throws IOException {
-            try (FileWriter fileWriter = new FileWriter(filePath)) {
-                for (String line : lines) {
-                    fileWriter.write(line + System.lineSeparator());
-                }
+    public static void overwriteFile(String filePath, String[] data) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (String line : data) {
+                writer.write(line);
+                writer.newLine();
             }
         }
     }
-
- */
 }
