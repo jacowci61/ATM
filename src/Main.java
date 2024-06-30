ae2044714b392e341370f29eacb2dcb9da17943d
jacowci61 <jacowci61@gmail.com>
2024-06-30 23:47:40 +0300

    Added class object creation inside a loop

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
             String line;
             System.out.println("\n");
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+                //System.out.println(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,10 +45,25 @@ public class Main {
             String[] lines = readFileIntoArray(filePathTEST);
 
             System.out.println("\n");
+            List<BankAccount> BankAccountsList = new ArrayList<BankAccount>(lines.length);
+            List<CreditCard> CreditCardsList = new ArrayList<CreditCard>(lines.length);
+
             for (String line : lines) {
-                System.out.println("Lines in txt file are: " + line);
+                //System.out.println("Lines in txt file are: " + line);
+                String[] TempObject = line.split("\\s+");
+                BankAccount data1 = new BankAccount(TempObject[0], Double.parseDouble(TempObject[1]));
+                BankAccountsList.add(data1);
+
+                CreditCard data2 = new CreditCard(data1, Long.parseLong(TempObject[2]), Integer.parseInt(TempObject[3]), Boolean.parseBoolean(TempObject[4]));
+                CreditCardsList.add(data2);
             }
 
+            for (int i = 0; i < BankAccountsList.size(); i++) {
+                System.out.println(BankAccountsList.get(i).toString());
+            }
+            for (int i = 0; i < CreditCardsList.size(); i++) {
+                System.out.println(CreditCardsList.get(i).getCreditCardNumber());
+            }
             String strTEST = lines[0];
             System.out.println("\n");
             System.out.println(strTEST);
